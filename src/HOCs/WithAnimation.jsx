1ab3c43cf593f4d4514967baa6a39ce839fc54d4
TheNear <thenearx@gmail.com>
2020-05-10 01:07:18 +0300

    add proptypes to HOC withAnimation

--- src/HOCs/WithAnimation.jsx
+++ src/HOCs/WithAnimation.jsx
@@ -1,26 +1,29 @@
 import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 
-export default function WithAnimation(Component) {
+const WithAnimation = (Component) => {
   const WithAnimationWrap = (props) => {
+    const { show, duration } = props;
     const [isMounted, setIsMounted] = useState(false);
 
     useEffect(() => {
-      const timeout = setTimeout(
-        () => setIsMounted(props.show),
-        props.show ? 0 : props.duration
-      );
+      const timeout = setTimeout(() => setIsMounted(show), show ? 0 : duration);
       return () => clearTimeout(timeout);
-    }, [props.show, props.duration]);
+    }, [show, duration]);
 
     return (
-      (isMounted || props.show) && (
-        <Component
-          {...props}
-          show={isMounted && props.show}
-          setShow={setIsMounted}
-        />
+      (isMounted || show) && (
+        <Component {...props} show={isMounted && show} setShow={setIsMounted} />
       )
     );
   };
+
+  WithAnimationWrap.propTypes = {
+    show: PropTypes.bool.isRequired,
+    duration: PropTypes.number.isRequired,
+  };
+
   return WithAnimationWrap;
-}
+};
+
+export default WithAnimation;

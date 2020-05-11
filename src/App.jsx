7241daab7575ab173775fb04fc9a74ac737a7cc7
TheNear <thenearx@gmail.com>
2020-05-11 20:59:30 +0300

    Start work with firebase

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
-
 import MainPage from "./pages/MainPage";
 import ModalPopupGroup from "./componentHelpers/ModalPopupGroup";
 import AuthPage from "./pages/AuthPage";
